044d1c3944e684978ebfff559e31aaa3592d1ccc
mikerouw <54872376+mikerouw@users.noreply.github.com>
2019-11-09 11:43:48 -0600

    Add files via upload
    Bar chart for phenophase durations

---
+++ Data/Plots/GGPLOT_byDuration.R
@@ -0,0 +1,7 @@
+ggplot(JeanFinalDataSet,aes(x=YesDurationGroup)) +
+  + geom_bar(color="blue",fill="blue") +
+  + labs(title="Duration of Positive Phenophase Observation", y="Number of Observations",x="Duration Groups")
+
+
+
+
